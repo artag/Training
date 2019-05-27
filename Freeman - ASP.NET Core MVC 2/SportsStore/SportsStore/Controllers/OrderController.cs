b771d196b303e46327ec5bc4120733d2b65e010b
Artem Agafonov <aae@mtl.ru>
2019-05-27 18:40:51 +0300

    Freeman: Finished chapter 10.

--- Freeman - ASP.NET Core MVC 2/SportsStore/SportsStore/Controllers/OrderController.cs
+++ Freeman - ASP.NET Core MVC 2/SportsStore/SportsStore/Controllers/OrderController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 
@@ -5,9 +6,41 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Checkout()
+        private readonly IOrderRepository _orderRepository;
+        private readonly Cart _cartService;
+
+        public OrderController(IOrderRepository orderRepository, Cart cartService)
         {
-            return View(new Order());
+            _orderRepository = orderRepository;
+            _cartService = cartService;
+        }
+
+        public IActionResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (!_cartService.Lines.Any())
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cartService.Lines.ToArray();
+                _orderRepository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            _cartService.Clear();
+            return View();
         }
     }
 }
