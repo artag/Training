0b36219ff6defba2002a7aedc77c4692d1a8807d
Artem Agafonov <aae@mtl.ru>
2019-05-27 19:24:32 +0300

    Freeman: Chapter 11. Stopped on AdminControllerTests (not finished yet).

---
+++ Freeman - ASP.NET Core MVC 2/SportsStore/SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void Index_Contains_All_Products()
+        {
+            // Arrange
+            var products = new[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+            };
+
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(products).AsQueryable<Product>();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ Freeman - ASP.NET Core MVC 2/SportsStore/SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index() => View(_repository.Products);
+    }
+}
