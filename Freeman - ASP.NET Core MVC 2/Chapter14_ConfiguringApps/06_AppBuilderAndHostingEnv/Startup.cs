eeffa5b3473e64c7aeb55f271e82b6236734ce75
Artem Agafonov <agart@mail.ru>
2019-06-03 22:38:10 +0300

    Freeman: Chapter 14. Added information about IApplicationBuilder and IHostingEnvironment.

---
+++ Freeman - ASP.NET Core MVC 2/Chapter14_ConfiguringApps/06_AppBuilderAndHostingEnv/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppBuilderAndHostingEnv
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                if (env.IsDevelopment())
+                {
+                    await context.Response.WriteAsync("Development");
+                }
+                else if (env.IsStaging())
+                {
+                    await context.Response.WriteAsync("Staging");
+                }
+                else if (env.IsProduction())
+                {
+                    await context.Response.WriteAsync("Production");
+                }
+            });
+        }
+    }
+}
