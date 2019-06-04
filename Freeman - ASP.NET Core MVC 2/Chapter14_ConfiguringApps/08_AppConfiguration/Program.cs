60aa727576e4e33b79b2280c6a99fdbb4ff11b33
Artem Agafonov <agart@mail.ru>
2019-06-04 23:06:55 +0300

    Freeman: Chapter 14. Added information about AppConfiguration.

---
+++ Freeman - ASP.NET Core MVC 2/Chapter14_ConfiguringApps/08_AppConfiguration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace AppConfiguration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
+                   .UseKestrel()
+                   .UseContentRoot(Directory.GetCurrentDirectory())
+                   .ConfigureAppConfiguration(
+                       (hostingContext, config) =>
+                       {
+                           config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                           config.AddEnvironmentVariables();
+                           if (args != null)
+                           {
+                               config.AddCommandLine(args);
+                           }
+                       })
+                   .UseIISIntegration()
+                   .UseStartup<Startup>()
+                   .Build();
+        }
+    }
+}
---
+++ Freeman - ASP.NET Core MVC 2/Chapter14_ConfiguringApps/08_AppConfiguration/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppConfiguration
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                var message = $"Readed Data from JSON file. 1st way: {ReadDataFirstWay()}\n" +
+                              $"Readed Data from JSON file. 2nd way: {ReadDataSecondWay()}\n" +
+                              $"Readed Data from JSON file. 3rd way: {ReadDataThirdWay()}\n" +
+                              "\n" +
+                              $"Readed first value from MySettings: {GetFirstValueFromMySettings()}\n" +
+                              $"Readed second value from MySettings: {GetSecondValueFromMySettings()}";
+
+                await context.Response.WriteAsync(message);
+            });
+        }
+
+        private bool ReadDataFirstWay()
+        {
+            IConfigurationSection section = _configuration.GetSection("ShortCircuitMiddleware");
+            bool? value = section?.GetValue<bool>("EnableBrowserShortCircuit");
+            return value.Value;
+        }
+
+        private bool ReadDataSecondWay()
+        {
+            return (_configuration.GetSection("ShortCircuitMiddleware")
+                                  ?.GetValue<bool>("EnableBrowserShortCircuit")).Value;
+        }
+
+        private string ReadDataThirdWay()
+        {
+            return _configuration["ShortCircuitMiddleware:EnableBrowserShortCircuit"];
+        }
+
+        private string GetFirstValueFromMySettings()
+        {
+            return _configuration.GetSection("MySettings:0").Value;
+        }
+
+        private string GetSecondValueFromMySettings()
+        {
+            return _configuration.GetSection("MySettings:1").Value;
+        }
+    }
+}
