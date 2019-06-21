cc9d5ad49e2b2111c08e7bef6b2bc418f1e439b2
Artem Agafonov <aae@mtl.ru>
2019-06-21 20:03:45 +0300

    Freeman: Chapter 17. Returning different types of content from actions.

---
+++ Freeman - ASP.NET Core MVC 2/Chapter17_ControllersAndActions/06_ReturningDifferentContent/ReturningDifferentContent/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReturningDifferentContent.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectToActionResult Index() => RedirectToAction("Index", "Json");
+    }
+}
---
+++ Freeman - ASP.NET Core MVC 2/Chapter17_ControllersAndActions/06_ReturningDifferentContent/ReturningDifferentContent/Controllers/JsonController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReturningDifferentContent.Controllers
+{
+    public class JsonController : Controller
+    {
+        public JsonResult Index() => Json(new[] { "Alice", "Bob", "Joe" });
+    }
+}
