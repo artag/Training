1d8bf4fbc9985d0a2cfa81bc6919c75fe8a45aa9
Artem Agafonov <agart@mail.ru>
2019-08-05 21:31:49 +0300

    AFPinCs: Implement a Mutable Shell.

---
+++ Applying Functional Principles in C#/FunctionalPrinciples/Immutability/ApplicationService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Immutability
+{
+    /// <summary>
+    /// Связующее звено между <see cref="AuditManager"/> и <see cref="Persister"/>.
+    /// </summary>
+    /// <remarks>
+    /// 1. Как можно более простой класс.
+    /// 2. Тестируется в Integration Tests (примерно 1-2 теста).
+    /// </remarks>
+    public class ApplicationService
+    {
+        private readonly string _directoryName;
+        private readonly AuditManager _auditManager;
+        private readonly Persister _persister;
+
+        public ApplicationService(string directoryName)
+        {
+            _directoryName = directoryName;
+        }
+
+        public void RemoveMentionsAbout(string visitorName)
+        {
+            var files = _persister.ReadDirectory(_directoryName);
+            var actions = _auditManager.RemoveMentionsAbout(visitorName, files);
+            _persister.ApplyChanges(actions);
+        }
+
+        public void AddRecord(string visitorName, DateTime timeOfVisit)
+        {
+            var fileInfo = new DirectoryInfo(_directoryName)
+                .GetFiles()
+                .OrderByDescending(f => f.LastWriteTime)
+                .First();
+
+            var file = _persister.ReadFile(fileInfo.Name);
+            var action = _auditManager.AddRecord(file, visitorName, timeOfVisit);
+            _persister.ApplyChange(action);
+        }
+    }
+}
---
+++ Applying Functional Principles in C#/FunctionalPrinciples/Immutability/Persister.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Immutability
+{
+    /// <summary>
+    /// Взаимодействие с файловой системой. Mutable.
+    /// </summary>
+    /// <remarks>
+    /// 1. Как можно более простой класс.
+    /// 2. Тестируется в Integration Tests (примерно 1-2 теста).
+    /// </remarks>
+    public class Persister
+    {
+        public FileContent ReadFile(string fileName)
+        {
+            return new FileContent(fileName, File.ReadAllLines(fileName));
+        }
+
+        public FileContent[] ReadDirectory(string directoryName)
+        {
+            return Directory
+                .GetFiles(directoryName)
+                .Select(file => ReadFile(file))
+                .ToArray();
+        }
+
+        public void ApplyChanges(IEnumerable<FileAction> actions)
+        {
+            foreach (var action in actions)
+            {
+                switch (action.Type)
+                {
+                    case ActionType.Create:
+                    case ActionType.Update:
+                        File.WriteAllLines(action.FileName, action.Content);
+                        continue;
+
+                    case ActionType.Delete:
+                        File.Delete(action.FileName);
+                        continue;
+
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public void ApplyChange(FileAction action)
+        {
+            ApplyChanges(new[] { action });
+        }
+    }
+}
