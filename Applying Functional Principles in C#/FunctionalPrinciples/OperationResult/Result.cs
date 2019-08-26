987e54cb849a03e9e183fc2f99ae6a4547d46249
Artem Agafonov <agart@mail.ru>
2019-08-26 23:52:04 +0300

    AFPinCs: Added OperationResult project.

---
+++ Applying Functional Principles in C#/FunctionalPrinciples/OperationResult/Result.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace OperationResult
+{
+    public class Result
+    {
+        protected Result(bool isSuccess, string error)
+        {
+            if (isSuccess && error != string.Empty)
+                throw new InvalidOperationException();
+
+            if (!isSuccess && error == string.Empty)
+                throw new InvalidOperationException();
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public string Error { get; }
+
+        public bool IsSuccess { get; }
+        
+        public bool IsFailure => !IsSuccess;
+
+        public static Result Fail(string message)
+        {
+            return new Result(false, message);
+        }
+
+        public static Result<T> Fail<T>(string message)
+        {
+            return new Result<T>(default(T), false, message);
+        }
+
+        public static Result Ok()
+        {
+            return new Result(true, string.Empty);
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>(value, true, string.Empty);
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        private readonly T _value;
+
+        protected internal Result(T value, bool isSuccess, string error)
+            : base(isSuccess, error)
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (!IsSuccess)
+                    throw new InvalidOperationException();
+
+                return _value;
+            }
+        }
+    }
+}
