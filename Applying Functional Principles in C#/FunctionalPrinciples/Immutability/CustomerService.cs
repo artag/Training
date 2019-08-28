63b74b6e10cc3135226d365b3dcd986df05dfb03
Artem Agafonov <agart@mail.ru>
2019-08-28 22:51:34 +0300

    AFPinCs: Refactor CustomerService class to immutable state.

--- Applying Functional Principles in C#/FunctionalPrinciples/Immutability/CustomerService.cs
+++ Applying Functional Principles in C#/FunctionalPrinciples/Immutability/CustomerService.cs
@@ -2,33 +2,27 @@
 {
     public class CustomerService
     {
-        private Address _address;
-        private Customer _customer;
-
         public void Process(string customerName, string addressString)
         {
-            // Проблема: можно вызвать методы в другом порядке.
-            CreateAddress(addressString);
-            CreateCustomer(customerName);
-            SaveCustomer();
+            var address = CreateAddress(addressString);
+            var customer = CreateCustomer(customerName, address);
+            SaveCustomer(customer);
         }
 
-        // Проблема: метод изнутри меняет поле _address.
-        private void CreateAddress(string addressString)
+        private Address CreateAddress(string addressString)
         {
-            _address = new Address(addressString);
+            return new Address(addressString);
         }
 
-        // Проблема: метод изнутри меняет поле _customer.
-        private void CreateCustomer(string customerName)
+        private Customer CreateCustomer(string customerName, Address address)
         {
-            _customer = new Customer(customerName, _address);
+            return new Customer(customerName, address);
         }
 
-        private void SaveCustomer()
+        private void SaveCustomer(Customer customer)
         {
             var repository = new Repository();
-            repository.Save(_customer);
+            repository.Save(customer);
         }
     }
 }
