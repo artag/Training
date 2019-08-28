218bea0f5eab580a1fc0edfc1db8afc2c877fdb9
Artem Agafonov <agart@mail.ru>
2019-08-28 22:19:16 +0300

    AFPinCs: Added UserProfile class to Immutability project. For refactoring.

---
+++ Applying Functional Principles in C#/FunctionalPrinciples/Immutability/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace Immutability
+{
+    public class UserProfile
+    {
+        private User _user;
+        private string _address;
+
+        // Здесь side effect
+        public void UpdateUser(int userId, string name)
+        {
+            _user = new User(userId, name);
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
