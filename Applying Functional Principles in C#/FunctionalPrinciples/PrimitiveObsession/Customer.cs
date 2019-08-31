fda9f7aac05d2382ab08cff8b3efbc5e43cac25f
Artem Agafonov <agart@mail.ru>
2019-08-31 22:22:28 +0300

    AFPinCs: Added two classes. CustomerService and Customer. Ready to refactoring.

---
+++ Applying Functional Principles in C#/FunctionalPrinciples/PrimitiveObsession/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PrimitiveObsession
+{
+    public class Customer
+    {
+        public Customer(string name, string email)
+        {
+            ChangeName(name);
+            ChangeEmail(email);
+        }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        private void ChangeName(string name)
+        {
+            // Проблема 1. Code smell. Customer не должен проверять правильность name.
+            // Проблема 2. Сигнатура этого метода dishonest.
+
+            // Validate name
+            if (string.IsNullOrWhiteSpace(name) || name.Trim().Length > 50)
+                throw new ArgumentException("Name is invalid");
+
+            Name = name;
+        }
+
+        private void ChangeEmail(string email)
+        {
+            // Проблема 1. Code smell. Customer не должен проверять правильность e-mail.
+            // Проблема 2. Сигнатура этого метода dishonest.
+
+            // Validate e-mail
+            if (string.IsNullOrWhiteSpace(email) || email.Length > 100)
+                throw new ArgumentException("E-mail is invalid");
+
+            if (!Regex.IsMatch(email, @"^(.+)@(.+)$"))
+                throw new ArgumentException("Email is invalid");
+
+            Email = email;
+        }
+    }
+}
---
+++ Applying Functional Principles in C#/FunctionalPrinciples/PrimitiveObsession/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimitiveObsession
+{
+    public class CustomerController : Controller
+    {
+        private readonly IDatabase _database;
+
+        public CustomerController(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCustomer(CustomerModel customerModel)
+        {
+            if (!ModelState.IsValid)
+                return View("Error");
+
+            var customer = new Customer(customerModel.Name, customerModel.Email);
+            _database.Save(customer);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class CustomerModel
+    {
+        // Проблема. Дублирование проверок в CustomerModel и Customer классах.
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name is too long")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "E-mail is required")]
+        [RegularExpression(@"^(.+)@(.+)$", ErrorMessage = "Invalid e-mail address")]
+        public string Email { get; set; }
+    }
+}
