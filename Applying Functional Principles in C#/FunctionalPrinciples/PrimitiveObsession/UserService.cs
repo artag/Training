d3618b69977c198de80a0173fd6ca78f4fb7b853
Artem Agafonov <agart@mail.ru>
2019-08-31 20:54:17 +0300

    AFPinCs: Introduce UserService class containing copy-paste code. Ready to refactoring.

---
+++ Applying Functional Principles in C#/FunctionalPrinciples/PrimitiveObsession/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrimitiveObsession
+{
+    public class UserService
+    {
+        public void ProcessUser(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(nameof(name));
+
+            if (name.Trim().Length > 100)
+                throw new ArgumentException(nameof(name));
+
+            // Processing code
+        }
+
+        public void CreateUser(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(nameof(name));
+
+            if (name.Trim().Length > 100)
+                throw new ArgumentException(nameof(name));
+
+            // Creation code
+        }
+
+        public void UpdateUser(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(nameof(name));
+
+            if (name.Trim().Length > 100)
+                throw new ArgumentException(nameof(name));
+
+            // Update code
+        }
+    }
+}
