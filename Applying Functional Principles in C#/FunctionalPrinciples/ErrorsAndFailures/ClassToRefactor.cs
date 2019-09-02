0e6860a5ab725735e7ab8300ffb7ca626731a30d
Artem Agafonov <agart@mail.ru>
2019-09-02 22:36:01 +0300

    AFPinCs: Add to ClassToRefactor various checks.

--- Applying Functional Principles in C#/FunctionalPrinciples/ErrorsAndFailures/ClassToRefactor.cs
+++ Applying Functional Principles in C#/FunctionalPrinciples/ErrorsAndFailures/ClassToRefactor.cs
@@ -1,24 +1,65 @@
-﻿namespace ErrorsAndFailures
+﻿using System;
+
+namespace ErrorsAndFailures
 {
     public class ClassToRefactor
     {
         private readonly IDatabase _database;
         private readonly IPaymentGateway _paymentGateway;
+        private readonly ILogger _logger;
 
-        public ClassToRefactor(IDatabase database, IPaymentGateway paymentGateway)
+        public ClassToRefactor(IDatabase database, IPaymentGateway paymentGateway, ILogger logger)
         {
             _database = database;
             _paymentGateway = paymentGateway;
+            _logger = logger;
         }
 
         public string RefillBalance(int customerId, decimal moneyAmount)
         {
-            Customer customer = _database.GetById(customerId);
-            customer.Balance += moneyAmount;
-            _paymentGateway.ChargePayment(customer.BillingInfo, moneyAmount);
-            _database.Save(customer);
+            if (!IsMoneyAmountValid(moneyAmount))
+            {
+                _logger.Log("Money amount is invalid");
+                return "Money amount is invalid";
+            }
 
+            Customer customer = _database.GetById(customerId);
+            if (customer == null)
+            {
+                _logger.Log("Customer is not found");
+                return "Customer is not found";
+            }
+
+            customer.Balance += moneyAmount;
+
+            try
+            {
+                _paymentGateway.ChargePayment(customer.BillingInfo, moneyAmount);
+            }
+            catch (ChargeFailedException ex)
+            {
+                _logger.Log("Unable to charge the credit card");
+                return "Unable to charge the credit card";
+            }
+
+            try
+            {
+                _database.Save(customer);
+            }
+            catch (Exception e)
+            {
+                _paymentGateway.RollbackLastTransaction();
+                _logger.Log("Unable to connect to the database");
+                return "Unable to connect to the database";
+            }
+
+            _logger.Log("OK");
             return "OK";
+        }
+
+        private bool IsMoneyAmountValid(decimal moneyAmount)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
---
+++ Applying Functional Principles in C#/FunctionalPrinciples/ErrorsAndFailures/Mock/ChargeFailedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ErrorsAndFailures
+{
+    public class ChargeFailedException : Exception
+    {
+        public ChargeFailedException()
+        {
+        }
+
+        public ChargeFailedException(string message)
+            : base(message)
+        {
+        }
+
+        public ChargeFailedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
