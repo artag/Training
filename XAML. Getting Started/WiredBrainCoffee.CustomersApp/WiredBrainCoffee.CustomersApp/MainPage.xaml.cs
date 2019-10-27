2dcd85b459cdb2a96b639efbbc8559d920483431
Artem Agafonov <agart@mail.ru>
2019-10-27 16:36:27 +0300

    XAML_Getting_Started: 05. Accessing Objects from Code.
    Synchronize the Detail Controls in C#.

--- XAML. Getting Started/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ XAML. Getting Started/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -72,5 +72,37 @@ namespace WiredBrainCoffee.CustomersApp
 
             moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
         }
+
+        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+
+            txtFirstName.Text = customer?.FirstName ?? string.Empty;
+            txtLastName.Text = customer?.LastName ?? string.Empty;
+            chkIsDeveloper.IsChecked = customer?.IsDeveloper;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer == null)
+            {
+                return;
+            }
+
+            customer.FirstName = txtFirstName.Text;
+            customer.LastName = txtLastName.Text;
+            customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+        }
     }
 }
