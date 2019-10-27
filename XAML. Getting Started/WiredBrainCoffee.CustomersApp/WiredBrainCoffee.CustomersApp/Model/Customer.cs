6f6cbfae5bc5dc32dd2050599cf5a1d807dabdfa
Artem Agafonov <agart@mail.ru>
2019-10-27 17:13:23 +0300

    XAML_Getting_Started: 05. Accessing Objects from Code.
    Notify About Property Changes. INotifyPropertyChanged implementation.

--- XAML. Getting Started/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/Model/Customer.cs
+++ XAML. Getting Started/WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/Model/Customer.cs
@@ -1,11 +1,41 @@
-﻿namespace WiredBrainCoffee.CustomersApp.Model
+﻿using WiredBrainCoffee.CustomersApp.Base;
+
+namespace WiredBrainCoffee.CustomersApp.Model
 {
-    public class Customer
+    public class Customer : Observable
     {
-        public string FirstName { get; set; }
+        private string _firstName;
+        private string _lastName;
+        private bool _isDeveloper;
 
-        public string LastName { get; set; }
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public bool IsDeveloper { get; set; }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsDeveloper
+        {
+            get => _isDeveloper;
+            set
+            {
+                _isDeveloper = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
