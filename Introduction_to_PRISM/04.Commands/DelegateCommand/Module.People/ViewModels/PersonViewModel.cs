7285884cfbad10bb6b5092ce2830672e163b320f
Artem Agafonov <agart@mail.ru>
2020-02-27 22:45:51 +0300

    Intro_Prism: Preparing for creation of DelegateCommand example.

---
+++ Introduction_to_PRISM/04.Commands/DelegateCommand/Module.People/ViewModels/PersonViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Demo.Business;
+using Demo.Infrastructure;
+using Module.People.Views;
+
+namespace Module.People.ViewModels
+{
+    public class PersonViewModel : ViewModelBase, IPersonViewModel
+    {
+        private Person _person;
+
+        public PersonViewModel(IPersonView view) : base(view)
+        {
+            CreatePerson();
+        }
+
+        public Person Person
+        {
+            get => _person;
+            set
+            {
+                _person = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void CreatePerson()
+        {
+            Person = new Person()
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                Age = 46,
+            };
+        }
+    }
+}
