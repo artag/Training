b8c028ad9bf48f47d8b2bcf4f33cb99f0b2da5d2
Artem Agafonov <agart@mail.ru>
2020-03-17 22:31:04 +0300

    Intro_Prism: Preparing for Event Aggregation.

---
+++ Introduction_to_PRISM/05.EventAggregator/EventAggregator/Demo.People/ViewModels/PersonViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using Demo.Business;
+using Demo.Infrastructure;
+using Demo.People.Views;
+using Prism.Commands;
+
+namespace Demo.People.ViewModels
+{
+    public class PersonViewModel : ViewModelBase, IPersonViewModel
+    {
+        private Person _person;
+
+        public PersonViewModel(IPersonView view) : base(view)
+        {
+            SaveCommand = new DelegateCommand(Save, CanSave);
+            GlobalCommands.SaveAllCommand.RegisterCommand(SaveCommand);
+        }
+
+        public DelegateCommand SaveCommand { get; }
+
+        public string ViewName => $"{Person.LastName} {Person.FirstName}";
+
+        public Person Person
+        {
+            get => _person;
+            set 
+            {
+                if (_person != null)
+                    _person.PropertyChanged -= Person_PropertyChanged;
+
+                _person = value;
+                _person.PropertyChanged += Person_PropertyChanged;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public void CreatePerson(string firstName, string lastName, int age)
+        {
+            Person = new Person
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+            };
+        }
+
+        private void Person_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            SaveCommand.RaiseCanExecuteChanged();
+        }
+
+        private void Save()
+        {
+            Person.LastUpdated = DateTime.Now;
+        }
+
+        private bool CanSave()
+        {
+            return Person != null && Person.Error == null;
+        }
+    }
+}
---
+++ Introduction_to_PRISM/05.EventAggregator/EventAggregator/Demo.StatusBar/ViewModels/StatusBarViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Demo.Infrastructure;
+using Demo.StatusBar.Views;
+
+namespace Demo.StatusBar.ViewModels
+{
+    public class StatusBarViewModel : ViewModelBase, IStatusBarViewModel
+    {
+        private string message;
+
+        public StatusBarViewModel(IStatusBarView view) : base(view)
+        {
+        }
+
+        public string Message
+        {
+            get => message;
+            set
+            {
+                message = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
