75447e9f49798ffc403ad33462bca6cc1ebe31e8
Artem Agafonov <agart@mail.ru>
2021-02-28 16:29:54 +0300

    Book_F#: Add code and materials from lesson 27.

---
+++ Get Programming with F#/Interop/CSharpApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Model;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(4, "Supacars", new Tuple<double, double>(1.5, 3.4));
+            var brand = car.Brand;      // get-only
+            var wheels = car.Wheels;    // get-only
+
+            // Создание классов-подтипов Vehicle. Vehicle виден как abstract класс.
+            Car carItem = new Car(4, "Unknown", new Tuple<double, double>(1.7, 4));
+            Vehicle motorcar = Vehicle.NewMotorcar(carItem);
+            Vehicle motorbike = Vehicle.NewMotobike("Motor", 1.2);
+
+            // Использование motorcar.
+            if (motorcar.IsMotorcar)
+            {
+                var castedMotorcar = (Vehicle.Motorcar) motorcar;     // cast
+                Car innerCar = castedMotorcar.Item;                   // get-only
+                string innerCarBrand = innerCar.Brand;                // get-only
+                int motorcarTag = castedMotorcar.Tag;                 // 0
+            }
+
+            // Использование motorbike.
+            if (motorbike.IsMotobike)
+            {
+                var castedMotobike = (Vehicle.Motobike) motorbike;          // cast
+                string motobikeName = castedMotobike.Name;                  // get-only
+                double motobikeEngineSize = castedMotobike.EngineSize;      // get-only
+                int motobikeTag = castedMotobike.Tag;                       // 1
+            }
+
+            // Использование функции из F#.
+            // Calling a standard F# function from C#
+            var someCar = Functions.createCar(4, "SomeBrand", 1.5, 3.5);
+            // Calling a partially applied F# function from C#
+            var fourWheeledCar = Functions.createsFourWheeledCar
+                .Invoke("Supacars")
+                .Invoke(1.5)
+                .Invoke(3.5);
+            var br = fourWheeledCar.Brand;
+
+            // Создание и использование Record в неинициализированном состоянии.
+            var nonInitPerson = new Person();
+            var name = nonInitPerson.Name;      // null
+            var age = nonInitPerson.Age;        // 0
+
+            var initPerson = new Person("Sam", 18);
+            var knownName = initPerson.Name;
+            var knownAge = initPerson.Age;
+        }
+    }
+}
---
+++ Get Programming with F#/Interop/FSharpCode/Model.fs
@@ -0,0 +1,32 @@
+﻿namespace Model
+
+/// A standard F# record of a Car.
+type Car = {
+    /// The number of wheels on the car.
+    Wheels : int
+    /// The brand of the car.
+    Brand : string
+    /// The x/y of the car in meters.
+    Dimensions : float * float
+    }
+
+/// A vehicle of some sort.
+type Vehicle =
+    /// A car is a type of vehicle.
+    | Motorcar of Car
+    /// A bike is also a type of vehicle.
+    | Motobike of Name : string * EngineSize : float
+
+module Functions =
+    /// Creates a car
+    let createCar wheels brand x y =
+        { Wheels = wheels; Brand = brand; Dimensions = x, y }
+
+    /// Creates a car with four wheels. (Curried function).
+    let createsFourWheeledCar = createCar 4
+
+[<CLIMutable>]
+type Person = {
+    Name : string
+    Age : int
+    }
