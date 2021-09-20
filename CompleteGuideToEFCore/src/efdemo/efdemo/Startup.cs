47bfc7423d6acb81ef03f1a42e06b9301b5ad322
Artem Agafonov <agart@mail.ru>
2021-09-20 21:52:26 +0300

    CGTEFC: Delete unused files by default.

--- CompleteGuideToEFCore/src/efdemo/efdemo/Startup.cs
+++ CompleteGuideToEFCore/src/efdemo/efdemo/Startup.cs
@@ -19,8 +19,6 @@ namespace efdemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
             // To use with SQLite database
             services.AddEntityFrameworkSqlite().AddDbContext<ApplicationDbContext>();
 
