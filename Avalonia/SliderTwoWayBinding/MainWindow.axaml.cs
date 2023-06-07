70d7193c385b5be648d0da3187728fc1347e7173
Artem Agafonov <agart@mail.ru>
2023-06-07 22:08:02 +0300

    Add SliderTwoWayBinding.

---
+++ Avalonia/SliderTwoWayBinding/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SliderTwoWayBinding
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
