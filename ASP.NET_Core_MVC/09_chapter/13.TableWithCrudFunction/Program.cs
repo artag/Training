23b6a13cdd729c6c6548289649f0f5d94636f748
Artem Agafonov <agart@mail.ru>
2023-09-23 20:27:45 +0300

    Chapter 9. Add buggy table crud example.

--- ASP.NET_Core_MVC/09_chapter/13.TableWithCrudFunction/Program.cs
+++ ASP.NET_Core_MVC/09_chapter/13.TableWithCrudFunction/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddMvc();
 builder.Services.AddSingleton<ICountries, Countries>();
 
 var app = builder.Build();
+app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
