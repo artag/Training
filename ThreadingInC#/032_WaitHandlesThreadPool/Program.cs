f342d6319742b5b340a99874e33fb6ab6e604c84
Artem Agafonov <agart@mail.ru>
2024-10-25 23:51:11 +0300

    Threading in C#. Add 32 and 33 examples.

---
+++ ThreadingInC#/032_WaitHandlesThreadPool/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    private static ManualResetEvent _starter = new ManualResetEvent(initialState: false);
+
+    public static void Main()
+    {
+        var reg = ThreadPool.RegisterWaitForSingleObject(
+            waitObject: _starter,
+            callBack: Go!,
+            state: "Data passed to object",
+            millisecondsTimeOutInterval: -1,
+            executeOnlyOnce: true);
+
+        Console.WriteLine("Main. Do some work");
+        Thread.Sleep(3000);
+
+        Console.WriteLine("Signaing worker...");
+        _starter.Set();
+
+        Console.WriteLine("Main. Finish work. Press 'Enter' to quit...");
+        Console.ReadLine();
+        reg.Unregister(waitObject: _starter);
+    }
+
+    private static void Go(object data, bool timeOut)
+    {
+        Console.WriteLine($"Started thread. Passed data: '{data}', timeout: {timeOut}");
+    }
+}
---
+++ ThreadingInC#/033_ContinueWithoutWaiting/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Program
+{
+    private static ManualResetEvent _wh = new ManualResetEvent(initialState: false);
+
+    public static void Main()
+    {
+        for (var i = 0; i < 2; i++)
+        {
+            var num = i;
+            new Thread(() => GoWithWait(num)).Start();
+        }
+        for (var i = 2; i < 4; i++)
+        {
+            var num = i;
+            new Thread(() => GoNoWait(num)).Start();
+        }
+
+        Console.WriteLine("Main thread start.");
+        Thread.Sleep(3000);
+
+        Console.WriteLine("Main thread call Set()");
+        _wh.Set();
+
+        Console.WriteLine("Main thread finish. Press 'Enter' to quit.");
+        Console.ReadLine();
+    }
+
+    private static void GoWithWait(int number)
+    {
+        Console.WriteLine($"Thread {number} start.");
+        MethodWithWait(number);
+        Console.WriteLine($"Thread {number} finish.");
+    }
+
+    private static void MethodWithWait(int number)
+    {
+        Console.WriteLine($"AppServerMethod {number} start.");
+        _wh.WaitOne();
+        Console.WriteLine($"AppServerMethod {number} finish.");
+    }
+
+    private static void GoNoWait(int number)
+    {
+        Console.WriteLine($"Thread {number} start.");
+        MethodNoWait(number);
+        Console.WriteLine($"Thread {number} finish.");
+    }
+
+    private static void MethodNoWait(int number)
+    {
+        Console.WriteLine($"AppServerMethod {number} start.");
+        var reg = ThreadPool.RegisterWaitForSingleObject(
+            waitObject: _wh,
+            callBack: Resume!,
+            state: number,
+            millisecondsTimeOutInterval: -1,
+            executeOnlyOnce: true);
+    }
+
+    private static void Resume(object data, bool timeOut)
+    {
+        var number = (int)data;
+        Console.WriteLine($"AppServerMethod {number} finish.");
+    }
+}
