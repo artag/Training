c08b96bbd22d027b8ad3257c6af0c2c5a14b29c6
Artem Agafonov <agart@mail.ru>
2024-10-26 23:43:56 +0300

    Threading in C#. Add WaitAny and WaitAll examples.

---
+++ ThreadingInC#/034_WaitAny/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    private static readonly WaitHandle[] _waitHandles = new WaitHandle[]
+    {
+        new AutoResetEvent(initialState: false),
+        new AutoResetEvent(initialState: false),
+    };
+
+    private static readonly Random _random = new Random();
+
+    public static void Main()
+    {
+        var dt = DateTime.Now;
+        ThreadPool.QueueUserWorkItem(new WaitCallback(DoTask!), _waitHandles[0]);
+        ThreadPool.QueueUserWorkItem(new WaitCallback(DoTask!), _waitHandles[1]);
+
+        var index = WaitHandle.WaitAny(_waitHandles);
+        Console.WriteLine(
+            "Task {0} finished first, time waited={1} ms",
+            index, (DateTime.Now - dt).TotalMilliseconds);
+    }
+
+    private static void DoTask(object state)
+    {
+        var are = (AutoResetEvent)state;
+        var time = 1000 * _random.Next(2, 10);
+        Console.WriteLine($"Performing a task for {time} ms");
+        Thread.Sleep(time);
+        are.Set();
+    }
+}
---
+++ ThreadingInC#/036_SignalAndWait/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Program
+{
+    private static readonly WaitHandle[] _waitHandles = new WaitHandle[]
+    {
+        new AutoResetEvent(initialState: false),
+        new AutoResetEvent(initialState: false),
+    };
+
+    private static readonly Random _random = new Random();
+
+    public static void Main()
+    {
+        // Толком непонятно, что делает WaitHandle.SignalAndWait
+
+        var dt = DateTime.Now;
+        ThreadPool.QueueUserWorkItem(
+            callBack: new WaitCallback(DoTask!),
+            state: new Data(_waitHandles[0], "Task #1"));
+        ThreadPool.QueueUserWorkItem(
+            callBack: new WaitCallback(DoTask!),
+            state: new Data(_waitHandles[1], "Task #2"));
+
+        WaitHandle.SignalAndWait(_waitHandles[1], _waitHandles[0]);
+
+        var timeWasted = (DateTime.Now - dt).TotalMilliseconds;
+        Console.WriteLine(
+            "Both tasks are completed, time waited={0} ms", timeWasted);
+
+        Console.WriteLine("Press 'Enter' to quit");
+        Console.ReadKey();
+    }
+
+    private static void DoTask(object state)
+    {
+        var data = (Data)state;
+        var are = (AutoResetEvent)data.AutoResetEvent;
+        var name = data.Name;
+
+        var time = 1000 * _random.Next(2, 10);
+        Console.WriteLine($"Performing a {name} for {time} ms");
+        Thread.Sleep(time);
+        are.Set();
+        Console.WriteLine($"Finish {name}");
+    }
+}
+
+internal record Data(
+    WaitHandle AutoResetEvent,
+    string Name
+);
