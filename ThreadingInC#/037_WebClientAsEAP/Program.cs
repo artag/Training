cafa8aede6b91dfe3e63c974035f2cee020907e5
Artem Agafonov <agart@mail.ru>
2024-10-27 18:05:35 +0300

    Threading in C#. Add 37, 38 examples.

---
+++ ThreadingInC#/037_WebClientAsEAP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Obsolete
+        var wc = new WebClient();
+        wc.DownloadStringCompleted += (sender, args) =>
+        {
+            if (args.Cancelled)
+                Console.WriteLine("Cancelled");
+            else if (args.Error != null)
+                Console.WriteLine("Exception: " + args.Error.Message);
+            else
+            {
+                Console.WriteLine(args.Result.Length + " chars were downloaded");
+                // We could update the UI from here...
+            }
+        };
+
+        wc.DownloadStringAsync(new Uri("http://google.com"));       // Start download
+
+        Console.WriteLine("Press 'Enter' to cancel download task and quit");
+        Console.ReadKey();
+    }
+}
