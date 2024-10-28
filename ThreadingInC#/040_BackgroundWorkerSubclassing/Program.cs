66ee2518cca227c131406309cf8b04b054e6bf5e
Artem Agafonov <agart@mail.ru>
2024-10-28 23:45:55 +0300

    Threading in C#. Add examples.

---
+++ ThreadingInC#/040_BackgroundWorkerSubclassing/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+
+public class Program
+{
+    public static void Main()
+    {
+        var client = new Client();
+        using var worker = client.GetFinancialWorkerBackground(1, 2);
+        worker.RunWorkerCompleted += (s, e) =>
+        {
+            if (e.Cancelled)
+                Console.WriteLine("Cancelled operation");
+            else if (e.Error != null)
+                Console.WriteLine("Error: " + e.Error.ToString());
+            else
+                Console.WriteLine("Result: " + e.Result);
+        };
+        worker.ProgressChanged += (s, e) =>
+        {
+            Console.WriteLine($"{e.ProgressPercentage}%    {e.UserState}");
+        };
+
+        worker.RunWorkerAsync();
+
+        Console.WriteLine("Press 'Enter' in the next 5 seconds to cancel");
+        Console.ReadLine();
+        if (worker.IsBusy)
+            worker.CancelAsync();
+        Console.ReadLine();
+    }
+}
+
+public class Client
+{
+    public FinancialWorker GetFinancialWorkerBackground(int foo, int bar)
+    {
+        return new FinancialWorker(foo, bar);
+    }
+}
+
+public class FinancialWorker : BackgroundWorker
+{
+    public FinancialWorker(int foo, int bar) : this()
+    {
+        Foo = foo;
+        Bar = bar;
+    }
+
+    public FinancialWorker()
+    {
+        WorkerReportsProgress = true;
+        WorkerSupportsCancellation = true;
+    }
+
+    public int Foo { get; }
+    public int Bar { get; }
+
+    protected override void OnDoWork(DoWorkEventArgs e)
+    {
+        for (var i = 0; i <= 90; i += 10)
+        {
+            if (CancellationPending)
+            {
+                Console.WriteLine("Cancel operation");
+                e.Cancel = true;
+                return;
+            }
+
+            Thread.Sleep(500);
+            ReportProgress(i, "Working");
+        }
+        ReportProgress(100, "Done!");
+        e.Result = 42;
+    }
+}
---
+++ ThreadingInC#/041_InterruptThread/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var t = new Thread(delegate()
+        {
+            try
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (ThreadInterruptedException ex)
+            {
+                Console.WriteLine("Interrupted: {0}", ex.Message);
+            }
+
+            Console.WriteLine("Finish thread");
+        });
+
+        t.Start();
+        t.Interrupt();
+    }
+}
