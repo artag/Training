0b815fa8118900f26098c86d2e5490f3c6880bb4
Artem Agafonov <agart@mail.ru>
2024-11-07 22:51:20 +0300

    Threading in C#. Add examples 59, 60.

---
+++ ThreadingInC#/059_ThreadUnsafeAndInterlockedDecrement/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var waitHandles = new List<WaitHandle>();
+
+        for (var i = 0; i < 10; i++)
+        {
+            var h1 = new EventWaitHandle(initialState: false, mode: EventResetMode.ManualReset);
+            var h2 = new EventWaitHandle(initialState: false, mode: EventResetMode.ManualReset);
+
+            waitHandles.Add(h1);
+            waitHandles.Add(h2);
+
+            new Thread(() => { ThreadUnsafe.Go(); h1.Set(); }).Start();
+            new Thread(() => { ThreadSafe.Go(); h2.Set(); }).Start();
+        }
+
+        WaitHandle.WaitAll(waitHandles.ToArray());
+
+        Console.WriteLine($"Unsafe decrement result = {ThreadUnsafe.X}");
+        Console.WriteLine($"Safe decrement result = {ThreadSafe.X}");
+    }
+}
+
+public static class ThreadUnsafe
+{
+    private static long _x = 1000L;
+
+    public static long X => _x;
+
+    public static void Go()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            _x--;
+        }
+    }
+}
+
+public static class ThreadSafe
+{
+    private static long _x = 1000L;
+
+    public static long X => _x;
+
+    public static void Go()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            Interlocked.Decrement(ref _x);
+        }
+    }
+}
---
+++ ThreadingInC#/060_InterlockedExamples/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var example = new InterlockedExample();
+        example.Run();
+    }
+}
+
+public class InterlockedExample
+{
+    private long _sum;
+
+    public void Run()
+    {
+        // Simple increment/decrement operations;
+        Interlocked.Increment(ref _sum);                    // 1
+        Interlocked.Decrement(ref _sum);                    // 0
+
+        // Add/substract a value;
+        Interlocked.Add(ref _sum, 3);                       // 3
+
+        // Read a 64-bit field
+        Console.WriteLine(Interlocked.Read(ref _sum));      // 3
+
+        // Write a 64-bit field while reading previous value.
+        // (This prints "3" while updating _sum to 10)
+        Console.WriteLine(Interlocked.Exchange(
+            location1: ref _sum,
+            value: 10));                                    // 10
+
+        // Update a field only if it matches a certain value (10).
+        // (This prints "10" while updating _sum to 123)
+        Console.WriteLine(Interlocked.CompareExchange(
+            location1: ref _sum,
+            value: 123,
+            comparand: 10));                                // 123
+
+        Console.WriteLine(_sum);        // Print "123"
+    }
+}
